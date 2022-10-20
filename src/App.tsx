c9431a05c9537c95f75d45d5a63838d38fce1650
Ricardo de Paula <flashdepaula@gmail.com>
2022-10-20 20:46:47 -0300

    feat: adding routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React, { Fragment } from 'react'
 import { StatusBar } from 'react-native'
-import { Login } from './screens/login/login'
+import { Routes } from './routes'
 
 export const App:React.FC = () => {
   return (
     <Fragment>
       <StatusBar barStyle='light-content'/>
-      <Login />
+      <Routes />
     </Fragment>
   )
 }
