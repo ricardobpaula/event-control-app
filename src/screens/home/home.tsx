c849aa738c38bb1dc5178c9c3012073c400e812c
Ricardo de Paula <flashdepaula@gmail.com>
2022-10-31 14:54:25 -0300

    feat: adding loading events

--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 
 import { NativeStackNavigationProp } from "@react-navigation/native-stack"
 
@@ -10,10 +10,13 @@ import { Calendar, Content, Title } from './styles'
 import { SafeAreaView } from "../../styles/global"
 import { colors } from "../../styles/theme"
 import { useNavigation } from "@react-navigation/native"
+import { Loading } from "../../components/loading/loading"
+import { EventRepository } from "../../database/repositories/event-repository"
 
 type HomeScreenProp = NativeStackNavigationProp<HomeStackParamList, 'Home'>
 
 export const Home = () => {
+    const [loading, setLoading] = useState<boolean>(true)
     const [markedDates, setMarkedDates] = useState<MarkedDates>()
     const { user } = useAuth()
     const navigation = useNavigation<HomeScreenProp>()
@@ -24,6 +27,34 @@ export const Home = () => {
         })
     }
 
+    const loadAllEvents = async () => {
+        const realm = await EventRepository.start() 
+
+        try {
+            const events = realm.findMany('done = $0', false)
+            const dates = new Set(
+                events.map(
+                        item => item.date
+                                .toLocaleDateString('pt-BR')
+                                .replaceAll('/','-')))
+            
+            const markedDates = {}
+
+            dates.forEach(item => {
+                markedDates[item] = { marked: true }
+                console.log(markedDates[item])
+            })
+            
+            setMarkedDates({markedDates})
+            
+        } catch (error) {
+            console.log(error)
+        } finally {
+            realm.close()
+            setLoading(false)
+        }
+
+    }
     const hour = new Date().getHours()
     let salute = ''
 
@@ -38,21 +69,27 @@ export const Home = () => {
             salute = 'Boa noite'
             break;
     }
+
+    useEffect(() => {
+        loadAllEvents()
+    }, [])
+
+    if (loading) return <Loading />
+
     return (
         <SafeAreaView>
             <Content>
                 <Title>{salute} {user.name}</Title>
                 <Calendar
-                    markedDates={{'2022-10-22': {selected: true}
-                    ,'2022-10-30': {marked: true}}}
-                    markingType='period'
-                    pastScrollRange={12}
-                    futureScrollRange={12}
+                    markedDates={{'2022-10-30': {marked: true}}}
+                    markingType='dot'
+                    pastScrollRange={6}
+                    futureScrollRange={6}
                     onDayPress={value => handleOpenDay(value.timestamp)}
                     theme={{
                         calendarBackground: colors.gray[500],
                         textSectionTitleColor: colors.gray[100],
-                        todayTextColor: colors.gray[100],
+                        todayTextColor: colors.blue[500],
                         dayTextColor: colors.gray[100],
                         selectedDotColor: '#ffffff',
                         monthTextColor: colors.pink[700],
